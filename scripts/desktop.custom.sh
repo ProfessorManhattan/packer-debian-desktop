7d0b7202d8562109e74c13aeb58bba805dd49a20
Brian Zalewski <brian@megabyte.space>
2021-06-01 22:22:24 -0400

    Latest updates.

--- scripts/desktop.custom.sh
+++ scripts/desktop.custom.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Install the GNOME desktop
-sudo tasksel install desktop gnome-desktop
+export DEBIAN_FRONTEND="noninteractive"
 
-# Enable the GUI
-sudo systemctl set-default graphical.target
+# Install the GNOME desktop
+echo "Install Desktop environment"
+sudo apt-get -y install task-gnome-desktop --no-install-recommends
+echo "Ensure Grub has quiet splash setting"
+sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"' /etc/default/grub
+echo "Update Grub"
+sudo update-grub2
+echo "Install Plymouth"
+sudo apt-get -y install plymouth plymouth-themes
 
 # Reboot system for changes to take effect
+echo "Rebooting"
 sudo systemctl reboot
